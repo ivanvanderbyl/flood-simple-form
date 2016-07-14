7eebdc531e83d6156be2ebed3c8cb906e043f721
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-07-14 19:18:59 +0200

    Added isPromise mixin

---
+++ addon/utils/is-promise.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+const { typeOf } = Ember;
+
+function isPromiseLike(obj = {}) {
+  return typeOf(obj.then) === 'function' &&
+    typeOf(obj.catch) === 'function' &&
+    typeOf(obj.finally) === 'function';
+}
+
+export default function isPromise(obj) {
+  return isPromiseLike(obj);
+}
