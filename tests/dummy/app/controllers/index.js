0c5c10a08535c2abe4f615149cbbd7ebdb0105a4
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-07-21 13:05:07 +0200

    Updated example to use section

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import computed from 'ember-computed';
+import lookupValidator from 'ember-changeset-validations';
 import {
   validatePresence,
   validateLength,
@@ -7,7 +7,7 @@ import {
   validateInclusion,
 } from 'ember-changeset-validations/validators';
 
-const validations = {
+const UserValidations = {
   email: [
     validatePresence(true),
     validateFormat('email'),
@@ -16,55 +16,53 @@ const validations = {
     validatePresence(true),
     validateLength({ min: 8, max: 12 }),
   ],
-  country: validatePresence(true),
+};
+
+const PersonDetailValidations = {
+  country: [
+    validatePresence(true),
+    validateInclusion({ list: ['au', 'de'] }),
+  ],
   isAdmin: [
     validateInclusion({ list: [true] }),
+  ],
+  email: [
+    validatePresence(true),
+    validateFormat('email'),
   ],
 };
 
 export default Ember.Controller.extend({
-  user: {
-    email: 'user@example.com',
-    number: '',
-    country: { id: 'au', name: 'Australia' },
-    isAdmin: false,
-  },
+  UserValidations,
+  PersonDetailValidations,
+
+  user: null,
 
   countries: [
     { id: 'au', name: 'Australia' },
     { id: 'us', name: 'United States' },
     { id: 'nz', name: 'New Zealand' },
+    { id: 'de', name: 'Germany' },
   ],
 
   actions: {
-    validate({ key, newValue }) {
-      let validationsForKey = validations[key];
-
-      if (Ember.typeOf(validationsForKey) !== 'array') {
-        validationsForKey = [validationsForKey];
-      }
-
-      let i = -1;
-      let isValid = true;
-      while (++i < validationsForKey.length && isValid === true) {
-        let validatorFn = validationsForKey[i];
-        if (Ember.typeOf(validatorFn) === 'function') {
-          isValid = validatorFn(key, newValue);
-        }else {
-          isValid = true;
-        }
-      }
-
-      return isValid;
+    validate(options) {
+      return lookupValidator(UserValidations)(options);
     },
 
-    saveChanges(newData) {
-      return new Ember.RSVP.Promise((resolve) => {
-        setTimeout(() => {
-          Ember.setProperties(this.get('user'), newData);
-          resolve();
-        }, 1e3);
-      });
+    saveChanges(changeset) {
+      return changeset
+        .validate()
+        .then(() => {
+          if (changeset.get('isValid')) {
+            changeset.execute();
+            changeset.rollback();
+            return new Ember.RSVP.Promise((resolve) => {
+              setTimeout(() => resolve(), 1e3);
+            });
+          }
+        });
+
     },
 
     validateUser(attr, value) {
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      email: 'user@example.com',
+      number: '',
+      country: 'de',
+      isAdmin: null,
+      collectCountry: true,
+    };
+  },
+
+  setupController(controller, user) {
+    controller.setProperties({ user });
+  },
 });
