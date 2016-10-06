7c5fcabc460a33481b79a8494f66474bfc8c91c1
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-10-06 12:15:48 +1100

    Include polyfill for travis

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-  name: 'flood-simple-form'
+  name: 'flood-simple-form',
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+    target.options.babel = target.options.babel || { includePolyfill: true };
+  }
 };
